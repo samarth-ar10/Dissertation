ca58b575b393e70d2fd6d6be59a5eba479c3b95b
samarth-ar10 <samarth.ar10@gmail.com>
2024-09-04 01:24:35 +0530

    prepping dissertation submission

--- trial_of_server_part_17/unity_ros_link.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ~/workspaces/ur_gazebo/src
-git clone -b main-ros2  https://github.com/Unity-Technologies/ROS-TCP-Endpoint.git
-
-source install/setup.bash
-colcon build
-source install/setup.bash
